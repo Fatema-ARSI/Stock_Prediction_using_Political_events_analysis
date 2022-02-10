8eca5e69a80b1dbad4a1f77733db940a3568ee09
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-10 13:10:11 +0100

    Update get_predictions.py

--- get_predictions.py
+++ get_predictions.py
@@ -12,34 +12,46 @@ from pandas.core.reshape.merge import merge
 
 
 def get_predictions(data):
-    train_dates=pd.to_datetime(data['Date'])
+  #variables for training
+  cols=list(data)[1:11]
+  df_for_training=data[cols].astype(float)
+  train_dates=pd.to_datetime(data['Date'])
+  
+  scaler=sklearn.preprocessing.StandardScaler()
+  scaler=scaler.fit(df_for_training)
+  df_for_training_scaled=scaler.transform(df_for_training)
 
-    cols=list(data)[1:11]
-    df_for_training=data[cols].astype(float)
-    train_dates=pd.to_datetime(data['Date'])
+  train_x=[]
+  train_y=[]
+  n_future=1
+  n_past=90
 
-    model=load_model('stock_prediction.h5')
+  for i in range(n_past,len(df_for_training_scaled)-n_future+1):
+    train_x.append(df_for_training_scaled[i-n_past:i,0:df_for_training.shape[1]])
+    train_y.append(df_for_training_scaled[i+n_future-1:i+n_future,1])
 
-    forecast_period_dates=pd.date_range(list(train_dates)[-1],periods=n_futuredays,freq='1d').tolist()
+  train_x,train_y=np.array(train_x),np.array(train_y)
 
-    train_x=[]
+  model=keras.models.load_model('stock_prediction (1).h5')
+
+  
+  
+  forecast_period_dates=pd.date_range(list(train_dates)[-1],periods=n_futuredays,freq='1d').tolist()
+
+  forecast=model.predict(train_x[-n_futuredays:])
+
+  forecast_copies=np.repeat(forecast,df_for_training.shape[1],axis=-1)
+
+  y_pred_future=scaler.inverse_transform(forecast_copies)[:,0]
+
+  forecast_dates=[]
+  for time_i in forecast_period_dates:
+    forecast_dates.append(time_i.date())
+
+  df_forecast=pd.DataFrame({'Date':forecast_dates,'Prediction':y_pred_future})
+
+  return(df_forecast)
+  
 
 
-    for i in range(n_past,len(df_for_training_scaled)-n_future+1):
-      train_x.append(df_for_training_scaled[i-n_past:i,0:df_for_training.shape[1]])
-      train_y.append(df_for_training_scaled[i+n_future-1:i+n_future,1])
 
-
-    forecast=model.predict(train_x[-n_futuredays:])
-
-    forecast_copies=np.repeat(forecast,df_for_training.shape[1],axis=-1)
-
-    y_pred_future=scaler.inverse_transform(forecast_copies)[:,0]
-
-    forecast_dates=[]
-    for time_i in forecast_period_dates:
-        forecast_dates.append(time_i.date())
-
-    df_forecast=pd.DataFrame({'Date':forecast_dates,'Prediction':y_pred_future})
-
-    return(df_forecast)
