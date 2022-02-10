a3750d2c6eb2eadc265a64d7579c40ca7ebb9493
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-11 00:54:36 +0100

    Update part2.py

--- part2.py
+++ part2.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import yahoo_fin.stock_info as si
 import pandas as pd
 import datetime
+from functools import reduce
 import plotly.graph_objs as go
 import plotly.io as pio
 from plotly.offline import init_notebook_mode,iplot
@@ -77,6 +78,17 @@ class stock_prediction(HydraHeadApp):
         df_forecast3=get_predictions(df3)
         df_forecast4=get_predictions(df4)
         df_forecast5=get_predictions(df5)
+        
+        df_forecast=[df_forecast1,df_forecast2,df_forecast3,df_forecast4,df_forecast5]
+        
+        df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Date'],how='outer'), df_forecast)
+        df_merged.columns.values[1] = selected_stock[0]
+        df_merged.columns.values[2] = selected_stock[1]
+        df_merged.columns.values[3] =selected_stock[2]
+        df_merged.columns.values[4] = selected_stock[3]
+        df_merged.columns.values[5] =selected_stock[4]
+        
+
         
         ########@#####################################################################@
         #get the plot data
