974a762a550200647ebcf21b9e0f1c92fd124d30
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-12 00:02:22 +0100

    Update part1.py

--- part1.py
+++ part1.py
@@ -44,7 +44,10 @@ class regression_analysis(HydraHeadApp):
 
         ####################################################
         
-        st.write('This page showcase the Regression analysis performed for (selected stock) and stock market index S&P 500 returns to determine the relationship between (selected stock)’s daily returns and market index.')
+        #st.write(
+            
+            " - This page showcase the Regression analysis performed for " + str(selected_stock) + " and stock market index S&P 500 returns to determine the relationship between " +  str(selected_stock) + "’s daily returns and market index."
+        )
         
         ##Metrics Alpha Beta
         ticker_df['daily_ret'] = ticker_df['Close'].pct_change(1)
@@ -77,13 +80,13 @@ class regression_analysis(HydraHeadApp):
         col1.metric("Alpha", np.around(alpha,decimals=3), "")
         col2.metric("Beta", np.around(beta,decimals=3), "")
         
-        st.write('In the above metrics, the shown alpha and beta calculated using CAPM model, represents how stock well performed and its volatility compare to the market index.')
+        #st.write(' - In the above metrics, the shown alpha and beta calculated using CAPM model, represents how stock well performed and its volatility compare to the market index.')
         
-        st.write('How to interpret the numbers?')
+        #st.write(' ###### How to interpret the numbers?')
         
-        st.write('Alpha is represented as a number like 1 which means the stock performed better than market index by 1% and for the negative number like -4, its vice a versa. For as Beta, the base number is 1 indicating the volatility of the stock is exactly correlated with the market index and 1.5 its 50% mre volatile than the index.')
+        #st.write(' - Alpha is represented as a number like 1 which means the stock performed better than market index by 1% and for the negative number like -4, its vice a versa. For as Beta, the base number is 1 indicating the volatility of the stock is exactly correlated with the market index and 1.5 its 50% mre volatile than the index.')
         
-        st.write('The (selected stock) has performed (same as, badly, well) compare to S&P 500. Further in terms of volatility, it is (Positively Correlated, Negatively Correlated,) with the market which can be graphically demonstrated below:')
+        #st.write(" ###### The " + (selected stock)+" has performed " + str(alpha_result) + " compare to S&P 500. Further in terms of volatility, it is " + str(beta_result) + " the market which can be graphically demonstrated below:")
  
         
         ticker_df[selected_stock] = np.log(ticker_df['Adj Close'] / ticker_df['Adj Close'].shift(1))
@@ -105,7 +108,7 @@ class regression_analysis(HydraHeadApp):
         plt.legend()
         st.pyplot(plt)
         
-        st.write('To get the detailed summary click below')
+        #st.write("To get the detailed summary click below")
         
         if st.button('Linear Regression Summary'):
             st.text(slr_sm_model_ko.summary())
