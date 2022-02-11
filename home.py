7f27a85f16da6705c86f72250aa10b755195f0b4
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-12 00:21:41 +0100

    Update home.py

--- home.py
+++ home.py
@@ -22,9 +22,9 @@ class home_page(HydraHeadApp):
             f'<img src="data:image/gif;base64,{data_url}" alt="stock gif" width="750" height="500">',
             unsafe_allow_html=True,
         )
-        st.markdown(""" ## Can we predict the Stock Price and its movement in the future? """)
+        st.markdown(""" ###### Can we predict the Stock Price and its movement in the future? """)
         
-        st.markdown(""" ## Machine learning model helps to understand the affect of given news on the market stocks using sentiment analysis.
+        st.markdown(""" ###### Machine learning model helps to understand the affect of given news on the market stocks using sentiment analysis.
         This app showcase the predicted price using the news-events for the given day.
         Please select ** Regression on Stocks ** to compare the stock with Market Index S&P 500, ** Stock Price Prediction ** 
         to see selected stock's future price and **Portfolio Optimization** for allocating the capital to get higher return on selected portfolio
