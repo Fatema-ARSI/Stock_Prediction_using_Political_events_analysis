7550159a60b2f54b9c2260ba24664584b1cf72d8
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-11 01:44:09 +0100

    Update part2.py

--- part2.py
+++ part2.py
@@ -78,11 +78,6 @@ class stock_prediction(HydraHeadApp):
         df_forecast=[df_forecast1,df_forecast2,df_forecast3,df_forecast4,df_forecast5]
         
         df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Date'],how='outer'), df_forecast)
-        df_merged.columns.values[1] = selected_stock[0]
-        df_merged.columns.values[2] = selected_stock[1]
-        df_merged.columns.values[3] =selected_stock[2]
-        df_merged.columns.values[4] = selected_stock[3]
-        df_merged.columns.values[5] =selected_stock[4]
         
 
         
