ca7b2bd41a24428c44ad719df8e39e4817185110
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-11 01:40:20 +0100

    Update part2.py

--- part2.py
+++ part2.py
@@ -17,9 +17,7 @@ from hydralit import HydraHeadApp
 
 #create a wrapper class
 class stock_prediction(HydraHeadApp):
-
-#wrap all your code in this method and you should be done
-
+    #wrap all your code in this method and you should be done
     def run(self):
         ##############################################################
         ###SIDEBAR section
@@ -29,7 +27,7 @@ class stock_prediction(HydraHeadApp):
         selected_start_date='2014-01-02'
         selected_end_date='2022-01-01'
         num_company=st.sidebar.slider('Number of Stock Prediction To Show',1,5,2)
-
+        
         ####################################################
         ##today signals
         signal_data=today_signal(selected_stock)
@@ -41,7 +39,7 @@ class stock_prediction(HydraHeadApp):
                 tickers.append('HOLD')
             else :
                 tickers.append('SELL')
-
+                
         col1,col2,col3=st.columns(3)
         col1.metric(selected_stock[0],tickers[0],"")
         col2.metric(selected_stock[1],tickers[1],"")
@@ -49,30 +47,23 @@ class stock_prediction(HydraHeadApp):
         col1,col2=st.columns(2)
         col1.metric(selected_stock[3],tickers[3],"")
         col2.metric(selected_stock[4],tickers[4],"")
-
-
+        
         ########data download#################################################
-
         #get stock value
-       
-         df1 = get_self_made_data_frame(selected_stock[0],selected_start_date,selected_end_date)
-         df1.reset_index(inplace=True)
+        df1 = get_self_made_data_frame(selected_stock[0],selected_start_date,selected_end_date)
+        df1.reset_index(inplace=True)
         
-   
-         df2 = get_self_made_data_frame(selected_stock[1],selected_start_date,selected_end_date)
-         df2.reset_index(inplace=True)
+        df2 = get_self_made_data_frame(selected_stock[1],selected_start_date,selected_end_date)
+        df2.reset_index(inplace=True)
         
+        df3 = get_self_made_data_frame(selected_stock[2],selected_start_date,selected_end_date)
+        df3.reset_index(inplace=True)
         
-         df3 = get_self_made_data_frame(selected_stock[2],selected_start_date,selected_end_date)
-         df3.reset_index(inplace=True)
+        df4 = get_self_made_data_frame(selected_stock[3],selected_start_date,selected_end_date)
+        df4.reset_index(inplace=True)
         
-      
-         df4 = get_self_made_data_frame(selected_stock[3],selected_start_date,selected_end_date)
-         df4.reset_index(inplace=True)
-        
-      
-         df5 = get_self_made_data_frame(selected_stock[4],selected_start_date,selected_end_date)
-         df5.reset_index(inplace=True)
+        df5 = get_self_made_data_frame(selected_stock[4],selected_start_date,selected_end_date)
+        df5.reset_index(inplace=True)
 
         #################################################################
         
