5e6beb6d031564c0d9e84dadd061ede27cec8684
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-11 23:37:00 +0100

    Update home.py

--- home.py
+++ home.py
@@ -24,9 +24,11 @@ class home_page(HydraHeadApp):
         )
         st.markdown("""##
         Can we predict the Stock Price and its movement in the future?
+        
         Machine learning model helps to understand the affect of given news on the market stocks using sentiment analysis.
         This app showcase the predicted price using the news-events for the given day.
-        Please select **Regression on Stocks** to compare the stock with Market Index S&P 500, **Stock Price Prediction** to see selected stock's future price and **Portfolio Optimization** for allocating the capital to get higher return on selected portfolio from the navigation bar.
+        Please select **Regression on Stocks** to compare the stock with Market Index S&P 500, **Stock Price Prediction** 
+        to see selected stock's future price and **Portfolio Optimization** for allocating the capital to get higher return on selected portfolio from the navigation bar.
 
         --- Note: This is app can be used for information purpose only.
         * Python libraries: `Yahoo Finance`, `Pandas`, `Streamlit`, `Plotly`,`Hydralit`,`Tensorflow`, `Linear Regression`, `Pyportfolioopt`,`VaderSentiment`
