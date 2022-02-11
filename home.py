8cbd0ad9e7b6bc793074c60b51fbe5f80be77230
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-12 00:21:02 +0100

    Update home.py

--- home.py
+++ home.py
@@ -28,7 +28,7 @@ class home_page(HydraHeadApp):
         This app showcase the predicted price using the news-events for the given day.
         Please select ** Regression on Stocks ** to compare the stock with Market Index S&P 500, ** Stock Price Prediction ** 
         to see selected stock's future price and **Portfolio Optimization** for allocating the capital to get higher return on selected portfolio
-        from the navigation bar. """
+        from the navigation bar. 
 
          --- Note: This is app can be used for information purpose only.
         * Python libraries: `Yahoo Finance`, `Pandas`, `Streamlit`, `Plotly`,`Hydralit`,`Tensorflow`, `Linear Regression`, `Pyportfolioopt`,`VaderSentiment`  """)
