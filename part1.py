45488484dee249d6e660e4b8b28e2bd5fadfb872
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2022-02-11 21:52:16 +0100

    Update part1.py

--- part1.py
+++ part1.py
@@ -50,39 +50,4 @@ class regression_analysis(HydraHeadApp):
 
         LR = stats.linregress(ticker_df['daily_ret'].iloc[1:],index_df['daily_ret'].iloc[1:])
         beta,alpha,r_val,p_val,std_err = LR
-        alpha_result=[]
-        if alpha<0:
-            alpha_result='badly'
-        elif alpha>0:
-            alpha_result='well'
-        else:
-            alpha_result='same as'
-            
-        beta_result=[]
-        if beta==1:
-            beta_result='positively correlated with'
-        elif beta<0:
-            beta_result='negatively correlated with'
-        elif beta>1
-            beta_result='more volatile than'
-        else:
-            beta_result='no correlation'
-            
-        st.write(" ## This page showcase the Regression analysis performed for " + str(selected_stock) + "and stock market index S&P 500 returns to determine the relationship between " + str(selected_stock) + "’s daily returns and market index.')
-
-        with open('style.css') as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Alpha", np.around(alpha,decimals=3), "")
-        col2.metric("Beta", np.around(beta,decimals=3), "")
-        col3.metric("std_err", np.around(std_err,decimals=3), "")
-              
-                 
-        st.write(" #### In the above metrics, the shown alpha and beta calculated using CAPM model, represents how stock well performed and its volatility compare to the market index.")
-        st.write(" ### How to interpret the numbers?")
-        st.write("#### Alpha is represented as a number like 1 which means the stock performed better than market index by 1% and for the negative number like -4, its vice a versa. For as Beta, the base number is 1 indicating the volatility of the stock is exactly correlated with the market index and 1.5 its 50% mre volatile than the index.")
-        st.write("#### The " + "str(selected_stock) + " has performed "+ str(alpha_result)+ " compare to S&P 500. Further in terms of volatility, it is "+ str(beta_result)+ " the market which can be graphically demonstrated below: ")
-
-
         
