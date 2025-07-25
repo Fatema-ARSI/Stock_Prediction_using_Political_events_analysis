c34f236190837add6a643a06de6c600803ad04c1
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2025-07-25 16:45:37 +0200

    debug

--- part2.py
+++ part2.py
@@ -34,50 +34,6 @@ class stock_prediction(HydraHeadApp):
         selected_end_date='2022-01-01'
         num_company=st.sidebar.slider('Number of Stock Prediction To Show',1,5,2)
         
-        ####################################################
-        ##today signals
-        signal_data=today_signal(selected_stock)
-        tickers=[]
-        for i in signal_data:
-            if i==0.0:
-                tickers.append('HOLD')
-            elif i==1.0:
-                tickers.append('HOLD')
-            else :
-                tickers.append('SELL')
-                
-                
-                
-        st.write(" ##### Signal Indicator as of " + datetime.datetime.today().strftime('%Y-%m-%d') + " for selected stocks." )
-        st.write("")
-        st.write("")
-        st.write("")
-        st.write("")
         
-        
-        col1,col2,col3=st.columns(3)
-        col1.metric(selected_stock[0],tickers[0],"")
-        col2.metric(selected_stock[1],tickers[1],"")
-        col3.metric(selected_stock[2],tickers[2],"")
-        col1,col2=st.columns(2)
-        col1.metric(selected_stock[3],tickers[3],"")
-        col2.metric(selected_stock[4],tickers[4],"")
-        
-        ########data download#################################################
-        #get stock value
-        df1 = get_self_made_data_frame(selected_stock[0],selected_start_date,selected_end_date)
-        df1.reset_index(inplace=True)
-        
-        df2 = get_self_made_data_frame(selected_stock[1],selected_start_date,selected_end_date)
-        df2.reset_index(inplace=True)
-        
-        df3 = get_self_made_data_frame(selected_stock[2],selected_start_date,selected_end_date)
-        df3.reset_index(inplace=True)
-        
-        df4 = get_self_made_data_frame(selected_stock[3],selected_start_date,selected_end_date)
-        df4.reset_index(inplace=True)
-        
-        df5 = get_self_made_data_frame(selected_stock[4],selected_start_date,selected_end_date)
-        df5.reset_index(inplace=True)
 
         
