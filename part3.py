db6e09248762f9c053807f75cff1d32baac4db99
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2025-07-25 23:20:10 +0200

    Update part3.py

--- part3.py
+++ part3.py
@@ -89,6 +89,51 @@ class portfolio_optimization(HydraHeadApp):
         # Calculate the expected returns and the annualised sample covariance matrix of daily asset returns.
         mu = expected_returns.mean_historical_return(df)#returns.mean() * 252
         S = risk_models.sample_cov(df) #Get the sample covariance matrix
+                # Optimize for maximal Sharpe ration .
+        ef = EfficientFrontier(mu, S)
+        weights = ef.max_sharpe()
+        #Maximize the Sharpe ratio, and get the raw weights
+        cleaned_weights = ef.clean_weights()
+        
+        #Note the weights may have some rounding error, meaning they may not add up exactly to 1 but should be close
+        data=ef.portfolio_performance(verbose=True)
+
+        # Now we see that we can optimize this portfolio by having about 44.29% of the portfolio in Tesla , 55.71% in Amazon.
+        # Also I can see that the expected annual volatility has increased to 34.5% but the annual expected rate also to 50.7% is . This optimized portfolio has a Sharpe ratio of 1.41 which is good.
+        # I want to get the discrete allocation of each share of the stock, meaning I want to know exactly how many of each stock I should buy given some amount that I am willing to put into this portfolio.
+        latest_prices = get_latest_prices(df)
+        weights = cleaned_weights
+        da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=Amount)
+        allocation, leftover = da.lp_portfolio()
+        
+        for i in allocation:
+            st.write(i,allocation[i])
+            
+        st.markdown(""" **Funds remaining:** EURO {:.2f}""".format(leftover))
+        
+        
+        shares_allocations=pd.DataFrame(allocation.items(),columns=["Stocks","Shares"])
+        
+        labels=shares_allocations["Stocks"]
+        values=shares_allocations["Shares"]
+        colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen']
+        fig = go.Figure(data=[go.Pie(labels=labels,values=values)])
+        fig.update_traces(hoverinfo='label+value+percent', textinfo='value', textfont_size=20,
+                          marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+        st.plotly_chart(fig)
+        
+        st.write(""" **Expected annual return {:.2f}** """.format(data[0]*100)+'%')
+        
+        st.write(""" **Expected annual risk {:.2f}** """.format(data[1] * 100)+'%')
+        
+        st.write(""" **Sharpe Ratio {:.2f}** """.format(data[2])+'%')
+        
+        st.write('Sharper ratio is the average return earned in excess of the risk-free rate per unit of volatility or total risk. Usually, any Sharpe ratio greater than 1.0 is considered acceptable to good by investors. A ratio higher than 2.0 is rated as very good. A ratio of 3.0 or higher is considered excellent.')
+ 
+        
+        
+        if st.button('Portfolio Allocation Table'):
+            st.dataframe(shares_allocations)
 
 
                                  
