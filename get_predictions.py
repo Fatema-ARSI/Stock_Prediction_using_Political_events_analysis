40cad1b3811651592b5e1d55981ca3d09b15e11f
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2025-07-25 23:21:12 +0200

    Update get_predictions.py

--- get_predictions.py
+++ get_predictions.py
@@ -1,62 +1,52 @@
 import numpy as np
+
 import pandas as pd
 import datetime
+
 import tensorflow as tf
+
+
 from sklearn.preprocessing import StandardScaler
+from pandas.core.reshape.merge import merge
 
-def get_predictions(data, forecast_horizon=500):
-    # --- Prepare data ---
-    cols = list(data)[1:11]  # Feature columns, excluding 'Date'
-    df_features = data[cols].astype(float)
-    train_dates = pd.to_datetime(data['Date'])
-    
-    # Find index of 'Close' column in the features
-    close_idx = cols.index('Close')  # should be 3 as you said
 
-    # --- Scale features ---
-    scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df_features)
+def get_predictions(data):
+  #variables for training
+  cols=list(data)[1:11]
+  df_for_training=data[cols].astype(float)
+  train_dates=pd.to_datetime(data['Date'])
+  
+  scaler=StandardScaler()
+  scaler=scaler.fit(df_for_training)
+  df_for_training_scaled=scaler.transform(df_for_training)
 
-    # --- Load model ---
-    model = tf.keras.models.load_model('stock_prediction.h5')
+  train_x=[]
+  train_y=[]
+  n_future=1
+  n_past=90
 
-    # --- Initialize with last 90 days of actual data ---
-    n_past = 90
-    input_seq = df_scaled[-n_past:].copy()
+  for i in range(n_past,len(df_for_training_scaled)-n_future+1):
+    train_x.append(df_for_training_scaled[i-n_past:i,0:df_for_training.shape[1]])
+    train_y.append(df_for_training_scaled[i+n_future-1:i+n_future,1])
 
-    predictions = []
-    future_dates = []
+  train_x,train_y=np.array(train_x),np.array(train_y)
 
-    last_known_date = train_dates.iloc[-1]
+  model=tf.keras.models.load_model('stock_prediction.h5')
 
-    for i in range(forecast_horizon):
-        # Prepare input for model: shape (1, 90, features)
-        model_input = np.expand_dims(input_seq, axis=0)
+  
+  n_futuredays=500
+  forecast_period_dates=pd.date_range(list(train_dates)[-1],periods=n_futuredays,freq='1d').tolist()
 
-        # Predict scaled value for the Close column
-        pred_scaled = model.predict(model_input)[0]
+  forecast=model.predict(train_x[-n_futuredays:])
 
-        # To inverse transform, we create a fake scaled array with predicted Close at correct index
-        fake_scaled = np.zeros((1, df_features.shape[1]))
-        fake_scaled[0, close_idx] = pred_scaled[0]
+  forecast_copies=np.repeat(forecast,df_for_training.shape[1],axis=-1)
 
-        # Inverse transform to original scale
-        pred_unscaled = scaler.inverse_transform(fake_scaled)[0][close_idx]
+  y_pred_future=scaler.inverse_transform(forecast_copies)[:,0]
 
-        predictions.append(pred_unscaled)
-        future_dates.append((last_known_date + pd.Timedelta(days=i + 1)).date())
+  forecast_dates=[]
+  for time_i in forecast_period_dates:
+    forecast_dates.append(time_i.date())
 
-        # Prepare next input sequence: drop first row, append new row
-        # Replace Close value in new row with predicted scaled value
-        new_row = input_seq[-1].copy()
-        new_row[close_idx] = pred_scaled[0]
-        input_seq = np.vstack([input_seq[1:], new_row])
+  df_forecast=pd.DataFrame({'Date':forecast_dates,'Prediction':y_pred_future})
 
-    # --- Create result DataFrame ---
-    df_forecast = pd.DataFrame({
-        'Date': future_dates,
-        'Prediction': predictions
-    })
-
-    return df_forecast
-
+  return(df_forecast)
