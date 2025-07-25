d07eefba47d25a9ad763a8ef89f5510905a15894
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2025-07-25 20:41:13 +0200

    Update get_predictions.py

--- get_predictions.py
+++ get_predictions.py
@@ -4,11 +4,14 @@ import datetime
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
-def get_predictions(data, forecast_horizon=180):
+def get_predictions(data, forecast_horizon=252):
     # --- Prepare data ---
     cols = list(data)[1:11]  # Feature columns, excluding 'Date'
     df_features = data[cols].astype(float)
     train_dates = pd.to_datetime(data['Date'])
+    
+    # Find index of 'Close' column in the features
+    close_idx = cols.index('Close')  # should be 3 as you said
 
     # --- Scale features ---
     scaler = StandardScaler()
@@ -27,22 +30,26 @@ def get_predictions(data, forecast_horizon=180):
     last_known_date = train_dates.iloc[-1]
 
     for i in range(forecast_horizon):
-        # Prepare input for model
+        # Prepare input for model: shape (1, 90, features)
         model_input = np.expand_dims(input_seq, axis=0)
+
+        # Predict scaled value for the Close column
         pred_scaled = model.predict(model_input)[0]
 
-        # Inverse transform the predicted value
-        fake_scaled = np.repeat(pred_scaled.reshape(1, -1), df_features.shape[1], axis=-1)
-        pred_unscaled = scaler.inverse_transform(fake_scaled)[0][0]  # Assumes target is column 0
+        # To inverse transform, we create a fake scaled array with predicted Close at correct index
+        fake_scaled = np.zeros((1, df_features.shape[1]))
+        fake_scaled[0, close_idx] = pred_scaled[0]
+
+        # Inverse transform to original scale
+        pred_unscaled = scaler.inverse_transform(fake_scaled)[0][close_idx]
 
         predictions.append(pred_unscaled)
-        future_dates.append((last_known_date + pd.Timedelta(days=i+1)).date())
+        future_dates.append((last_known_date + pd.Timedelta(days=i + 1)).date())
 
-        # Prepare next input: shift and append new prediction
-        last_row = input_seq[-1].copy()
-        new_row = last_row.copy()
-        new_row[0] = pred_scaled[0]  # Replace only target feature
-
+        # Prepare next input sequence: drop first row, append new row
+        # Replace Close value in new row with predicted scaled value
+        new_row = input_seq[-1].copy()
+        new_row[close_idx] = pred_scaled[0]
         input_seq = np.vstack([input_seq[1:], new_row])
 
     # --- Create result DataFrame ---
@@ -52,3 +59,4 @@ def get_predictions(data, forecast_horizon=180):
     })
 
     return df_forecast
+
