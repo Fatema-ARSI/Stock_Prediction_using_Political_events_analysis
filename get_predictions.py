4232f7d549c8c830970475406b85642c38628e41
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2025-07-25 20:20:45 +0200

    new prediction method 6months

--- get_predictions.py
+++ get_predictions.py
@@ -1,59 +1,54 @@
 import numpy as np
-
 import pandas as pd
 import datetime
-
 import tensorflow as tf
-
-
 from sklearn.preprocessing import StandardScaler
-from pandas.core.reshape.merge import merge
 
+def get_predictions(data, forecast_horizon=180):
+    # --- Prepare data ---
+    cols = list(data)[1:11]  # Feature columns, excluding 'Date'
+    df_features = data[cols].astype(float)
+    train_dates = pd.to_datetime(data['Date'])
 
+    # --- Scale features ---
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df_features)
 
+    # --- Load model ---
+    model = tf.keras.models.load_model('stock_prediction.h5')
 
+    # --- Initialize with last 90 days of actual data ---
+    n_past = 90
+    input_seq = df_scaled[-n_past:].copy()
 
-def get_predictions(data):
-  #variables for training
-  cols=list(data)[1:11]
-  df_for_training=data[cols].astype(float)
-  train_dates=pd.to_datetime(data['Date'])
-  
-  scaler=StandardScaler()
-  scaler=scaler.fit(df_for_training)
-  df_for_training_scaled=scaler.transform(df_for_training)
+    predictions = []
+    future_dates = []
 
-  train_x=[]
-  train_y=[]
-  n_future=1
-  n_past=90
+    last_known_date = train_dates.iloc[-1]
 
-  for i in range(n_past,len(df_for_training_scaled)-n_future+1):
-    train_x.append(df_for_training_scaled[i-n_past:i,0:df_for_training.shape[1]])
-    train_y.append(df_for_training_scaled[i+n_future-1:i+n_future,1])
+    for i in range(forecast_horizon):
+        # Prepare input for model
+        model_input = np.expand_dims(input_seq, axis=0)
+        pred_scaled = model.predict(model_input)[0]
 
-  train_x,train_y=np.array(train_x),np.array(train_y)
+        # Inverse transform the predicted value
+        fake_scaled = np.repeat(pred_scaled.reshape(1, -1), df_features.shape[1], axis=-1)
+        pred_unscaled = scaler.inverse_transform(fake_scaled)[0][0]  # Assumes target is column 0
 
-  model=tf.keras.models.load_model('stock_prediction.h5')
+        predictions.append(pred_unscaled)
+        future_dates.append((last_known_date + pd.Timedelta(days=i+1)).date())
 
-  
-  n_futuredays=500
-  forecast_period_dates=pd.date_range(list(train_dates)[-1],periods=n_futuredays,freq='1d').tolist()
+        # Prepare next input: shift and append new prediction
+        last_row = input_seq[-1].copy()
+        new_row = last_row.copy()
+        new_row[0] = pred_scaled[0]  # Replace only target feature
 
-  forecast=model.predict(train_x[-n_futuredays:])
+        input_seq = np.vstack([input_seq[1:], new_row])
 
-  forecast_copies=np.repeat(forecast,df_for_training.shape[1],axis=-1)
+    # --- Create result DataFrame ---
+    df_forecast = pd.DataFrame({
+        'Date': future_dates,
+        'Prediction': predictions
+    })
 
-  y_pred_future=scaler.inverse_transform(forecast_copies)[:,0]
-
-  forecast_dates=[]
-  for time_i in forecast_period_dates:
-    forecast_dates.append(time_i.date())
-
-  df_forecast=pd.DataFrame({'Date':forecast_dates,'Prediction':y_pred_future})
-
-  return(df_forecast)
-  
-
-
-
+    return df_forecast
